8b9e950b6dc7b53c2b223107b99653dc5de7163f
silvanoamaral <silvano.amaral9@gmail.com>
2025-05-20 21:51:03 -0300

    feat: ad app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [AuthModule, UserModule, PrismaModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
